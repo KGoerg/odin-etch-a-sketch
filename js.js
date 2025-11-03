59f977f6af4d549b9e57eb6679982932f158afa0
kgoerg <224106412+KGoerg@users.noreply.github.com>
2025-11-03 12:21:41 -0600

    Attempt loop to create squares in grid

---
+++ js.js
@@ -0,0 +1,15 @@
+const container = document.querySelector("#container");
+
+function createSquares() {
+    for (i = 0; i < 16 * 16; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
+    }
+    square.style.height = "40px";
+    square.style.width = "40px";
+    square.style.border = "thin solid black";
+}
+
+createSquares();
+
